3e2e4ffe450723a84fcfb1157de3b2535ee0e6cb
tohinahmedrahat <rahat4248@gmail.com>
2022-11-24 18:27:52 +0600

    add imagebb to host img

--- src/Page/Regester/Regester.js
+++ src/Page/Regester/Regester.js
@@ -4,26 +4,40 @@ import UseAuth from '../../Shared/UseAuth/UseAuth';
 import img from '../../img/regeter.webp'
 
 const Regester = () => {
-    const {loginWithGoogle,setUser,setError,error}= UseAuth()
-    const { register, handleSubmit,reset } = useForm();
+    const { loginWithGoogle, setUser, setError, error } = UseAuth()
+    const { register, handleSubmit, reset } = useForm();
     const imgHostKey = process.env.REACT_APP_imgbb_key
     const onSubmit = data => {
         setError("")
         const file = data.img[0]
-        console.log(file)
+        const formData = new FormData();
+        formData.append("image", file)
+        const url = `https://api.imgbb.com/1/upload?key=${imgHostKey}`
+        fetch(url, {
+            method: "POST",
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imgData => {
+                if (imgData.success) {
+                    console.log(imgData.data.url)
+                }
+            })
+            console.log(data)
         reset()
+
     };
-    const loginGoogle=() => {
+    const loginGoogle = () => {
         setError("")
         loginWithGoogle()
-        .then(result=>{
-            const user = result.user;
-            setUser(user)
-        })
-        .catch(error =>{
-            const errorMessage = error.message;
-            setError(errorMessage)
-        })
+            .then(result => {
+                const user = result.user;
+                setUser(user)
+            })
+            .catch(error => {
+                const errorMessage = error.message;
+                setError(errorMessage)
+            })
     }
     return (
         <div className='md:flex justify-center items-center gap-4'>
@@ -33,20 +47,24 @@ const Regester = () => {
                     <label className="label">
                         <span className="label-text">Type Your Name</span>
                     </label>
-                    <input placeholder='type your email' type="text" className="input input-bordered w-full" {...register("name")} />
+                    <input required placeholder='type your email' type="text" className="input input-bordered w-full" {...register("name")} />
                     <label className="label">
                         <span className="label-text">Upload Your Photo</span>
                     </label>
-                    <input type="file" {...register("img",{required:true})} />
+                    <input type="file" required {...register("img")} />
+                    <select className="input input-bordered w-full my-3" required {...register("role")}>
+                        <option value="seller">Seller</option>
+                        <option value="buyer">Buyer</option>
+                    </select>
                     <label className="label">
                         <span className="label-text">Type Your Email?</span>
                     </label>
-                    <input placeholder='type your email' className="input input-bordered w-full" {...register("email")} />
+                    <input required placeholder='type your email' className="input input-bordered w-full" {...register("email")} />
                     <label className="label mt-3">
                         <span className="label-text">Type Your Password?</span>
                     </label>
-                    <input placeholder='type your password' className="input input-bordered w-full" type="password" {...register("password")} />
-                    {error && <p className='my-3 text-red-600'>{error}</p> }
+                    <input required placeholder='type your password' className="input input-bordered w-full" type="password" {...register("password")} />
+                    {error && <p className='my-3 text-red-600'>{error}</p>}
                     <input className="btn btn-outline my-3" type="submit" />
                 </form>
                 <button className="btn btn-outline my-3 w-full" onClick={loginGoogle}>Google</button>
