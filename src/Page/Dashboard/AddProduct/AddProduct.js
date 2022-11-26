6c9b5845ff3a9e0ef06f463675882f00742896ee
tohinahmedrahat <rahat4248@gmail.com>
2022-11-26 18:16:16 +0600

    add product to database

--- src/Page/Dashboard/AddProduct/AddProduct.js
+++ src/Page/Dashboard/AddProduct/AddProduct.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
+import UseAuth from '../../../Shared/UseAuth/UseAuth';
 
 const AddProduct = () => {
+    const {user} = UseAuth()
     const { register, handleSubmit, reset } = useForm();
     const [imgUrl,setImgUrl] = useState("")
     const imgHostKey = process.env.REACT_APP_imgbb_key
@@ -10,6 +12,14 @@ const AddProduct = () => {
         const file = data.img[0]
         const name = data.name;
         const details = data.details;
+        const price = data.price;
+        const number = data.number;
+        const buyYear = data.buyyear;
+        const category = data.category;
+        const condition = data.condition;
+        const meetplace = data.meetplace;
+        const email = user.email;
+        const date = new Date().toDateString()
         // host img to imgbb
         const formData = new FormData();
         formData.append("image", file)
@@ -24,17 +34,26 @@ const AddProduct = () => {
                     setImgUrl(imgData.data.url)
                 }
             })
-            const Category = {
+            const Categorys = {
                 name:name,
                 details:details,
-                img:imgUrl
+                img:imgUrl,
+                price:price,
+                number:number,
+                buyYear:buyYear,
+                category:category,
+                condition:condition,
+                meetplace:meetplace,
+                postTime:date,
+                userEmail:email
             }
-            fetch("http://localhost:5000/addCategory",{
+            
+            fetch("http://localhost:5000/addproduct",{
                 method:"POST",
                 headers:{
                     'content-type':'application/json'
                 },
-                body:JSON.stringify(Category)
+                body:JSON.stringify(Categorys)
             }).then(res => res.json())
             .then(data => {
                 if(data.acknowledged){
@@ -46,15 +65,55 @@ const AddProduct = () => {
     return (
         <div>
             <h4>add your product</h4>
-            <form onSubmit={handleSubmit(addProduct)} className="flex flex-col">
+            <form onSubmit={handleSubmit(addProduct)} className="flex flex-col ">
+            <label className="label">
+                    <span className="label-text text-lg capitalize">add product name</span>
+                </label>
+                <input className="input input-bordered w-full" type="text" {...register("name")} />
+            <label className="label">
+                    <span className="label-text text-lg capitalize">add product price</span>
+                </label>
+                <input className="input input-bordered w-full" type="text" {...register("price")} />
+            <label className="label">
+                    <span className="label-text text-lg capitalize">add contract number</span>
+                </label>
+                <input className="input input-bordered w-full" type="text" {...register("number")} />
+            <label className="label">
+                    <span className="label-text text-lg capitalize">Year of purchase</span>
+                </label>
+                <input className="input input-bordered w-full" type="text" {...register("buyyear")} />
                 <label className="label">
-                    <span className="label-text text-lg capitalize">Upload Your Photo</span>
+                    <span className="label-text text-lg capitalize">add product Photo</span>
                 </label>
                 <input className="bg-slate-600 py-2 px-1 rounded border-2 border-orange-400" type="file" {...register("img")} />
-                <select className="input input-bordered w-full my-3" required {...register("name")}>
+                <label className="label">
+                    <span className="label-text text-lg capitalize">add category</span>
+                </label>
+                <select className="input input-bordered w-full" required {...register("category")}>
                     <option value="cafe racer">Cafe Racer</option>
                     <option value="naked bikes">Naked Sports Bike</option>
                     <option value="sport bikes">Sport Bikes</option>
+                </select>
+                <label className="label">
+                    <span className="label-text text-lg capitalize">add condition</span>
+                </label>
+                <select className="input input-bordered w-full" required {...register("condition")}>
+                    <option value="excellent">excellent</option>
+                    <option value="good">good</option>
+                    <option value="fair">fair</option>
+                </select>
+                <label className="label">
+                    <span className="label-text text-lg capitalize">add meet place</span>
+                </label>
+                <select className="input input-bordered w-full" required {...register("meetplace")}>
+                    <option value="dhaka">Dhaka</option>
+                    <option value="chittagong">Chittagong</option>
+                    <option value="sylhet">Sylhet</option>
+                    <option value="Mymensingh">Mymensingh</option>
+                    <option value="Rangpur">Rangpur</option>
+                    <option value="Khulna"> Khulna</option>
+                    <option value="Rajshahi">Rajshahi</option>
+                    <option value="Barisal">Barisal</option>
                 </select>
                 <label className="label">
                     <span className="label-text text-lg capitalize">add some details</span>
